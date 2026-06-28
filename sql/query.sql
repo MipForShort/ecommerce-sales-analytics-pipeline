eeb5efa288dd80fe4d7b4e9bef4eff482ed65e7b
Mario Meade <mariomip98@gmail.com>
2026-06-28 14:40:30 -0600

    feat: sql file now has view for better perfomance on Power BI, created data folder to store .csv

--- sql/query.sql
+++ sql/query.sql
@@ -8,22 +8,32 @@ SELECT * FROM sales_data LIMIT 5;
 -- Which categories generate the highest total revenue? 
 -- This helps in focusing marketing efforts.
 
+-- CREATE VIEW
+CREATE OR REPLACE VIEW vw_category_profitability AS
+
 SELECT 
     "Category",
     ROUND(SUM("TotalRevenue"::NUMERIC), 2) AS total_revenue
 FROM sales_data
-GROUP BY "Category"
-ORDER BY total_revenue DESC;
+GROUP BY "Category";
+--ORDER BY total_revenue DESC;
+
+SELECT * FROM vw_category_profitability;
 
 -- Time-Series Trend Analysis
 -- What is the total revenue generated for each month of activity?
+
+-- CREATE VIEW
+CREATE OR REPLACE VIEW vw_time_trend_analysis AS
 
 SELECT
     DATE_TRUNC('month', "InvoiceDate") as month_trunc,
     ROUND(SUM("TotalRevenue"::NUMERIC), 2) as monthly_revenue
 FROM sales_data
-GROUP BY month_trunc
-ORDER BY month_trunc DESC;
+GROUP BY month_trunc;
+-- ORDER BY month_trunc DESC;
+
+SELECT * FROM vw_time_trend_analysis;
 
 -- ==================================================
 -- 2. CUSTOMER BEHAVIOR & SEGMENTATION
@@ -33,16 +43,24 @@ ORDER BY month_trunc DESC;
 -- What is the average spending per customer?
 -- This helps in understanding the value of the user base.
 
+-- CREATE VIEW
+CREATE OR REPLACE VIEW vw_customer_behavior_avg_ticket AS
+
 SELECT 
     "CustomerID",
     ROUND(AVG("TotalRevenue"::NUMERIC), 2) AS avg_customer_spend 
 FROM sales_data
-GROUP BY "CustomerID"
-ORDER BY avg_customer_spend DESC;
+GROUP BY "CustomerID";
+--ORDER BY avg_customer_spend DESC;
+
+SELECT * FROM vw_customer_behavior_avg_ticket;
 
 -- Advanced Filtering: High-Value Customers
 -- Get the ID of customers who have made at least 5 purchases
 -- and have an average purchase spend greater than 500 currency units.
+
+-- CREATE VIEW
+CREATE OR REPLACE VIEW vw_high_value_customers AS
 
 SELECT
     "CustomerID",
@@ -50,11 +68,16 @@ SELECT
     ROUND(AVG("TotalRevenue"::NUMERIC), 2) AS avg_spend
 FROM sales_data
 GROUP BY "CustomerID"
-HAVING COUNT(*) >= 5 AND AVG("TotalRevenue") > 500
-ORDER BY avg_spend DESC;
+HAVING COUNT(*) >= 5 AND AVG("TotalRevenue") > 500;
+--ORDER BY avg_spend DESC;
+
+SELECT * FROM vw_high_value_customers;
 
 -- Top 3 most purchased products within each category
 -- Identifies the best-selling items per category using window functions.
+
+-- CREATE VIEW
+CREATE OR REPLACE VIEW vw_top_products_by_category AS
 
 WITH item_count AS (
     SELECT
@@ -73,6 +96,8 @@ SELECT
 FROM item_count
 WHERE item_rank <= 3;
 
+SELECT * FROM vw_top_products_by_category;
+
 -- ==================================================
 -- 3. LOGISTICS & OPERATIONAL EFFICIENCY
 -- ==================================================
@@ -80,27 +105,40 @@ WHERE item_rank <= 3;
 -- Supply Chain Optimization
 -- Which shipment providers have the highest average shipping costs?
 
+-- CREATE VIEW
+CREATE OR REPLACE VIEW vw_highest_avg_shipping_cost AS
+
 SELECT 
     "ShipmentProvider",
     ROUND(AVG("ShippingCost"::NUMERIC), 2) as avg_shipping_cost
 FROM sales_data
-GROUP BY "ShipmentProvider"
-ORDER BY avg_shipping_cost DESC;
+GROUP BY "ShipmentProvider";
+--ORDER BY avg_shipping_cost DESC;
+
+SELECT * FROM vw_highest_avg_shipping_cost;
 
 -- Order Priority Analysis
 -- Is there a difference in average shipping costs based on OrderPriority?
 -- This may reveal if "High" priority orders are more costly to process.
 
+-- CREATE VIEW
+CREATE OR REPLACE VIEW vw_avg_shipping_cost_by_order_priority AS
+
 SELECT 
     "OrderPriority",
     ROUND(AVG("ShippingCost"::NUMERIC), 2) AS avg_shipping_cost
 FROM sales_data
-GROUP BY "OrderPriority"
-ORDER BY avg_shipping_cost DESC;
+GROUP BY "OrderPriority";
+--ORDER BY avg_shipping_cost DESC;
+
+SELECT * FROM vw_avg_shipping_cost_by_order_priority;
 
 -- Churn/Returns Analysis
 -- Which product category has the highest return rate?
 -- (i.e., what percentage of sales are marked as "Returned"?).
+
+-- CREATE VIEW
+CREATE OR REPLACE VIEW vw_category_return_rate AS
 
 SELECT
     "Category",
@@ -109,29 +147,41 @@ SELECT
         ELSE 0
     END)::NUMERIC / COUNT(*) * 100, 2) AS returned_status
 FROM sales_data
-GROUP BY "Category"
-ORDER BY returned_status DESC;
+GROUP BY "Category";
+--ORDER BY returned_status DESC;
+
+SELECT * FROM vw_category_return_rate;
 
 -- Shipping Cost vs. Return Status
 -- What is the average shipping cost for orders that were "Returned"
 -- compared to those that were "Not Returned"?
+
+-- CREATE VIEW
+CREATE OR REPLACE VIEW vw_avg_shipping_cost_for_returned AS
 
 SELECT
     "ReturnStatus",
     ROUND(AVG("ShippingCost"::NUMERIC), 2) AS avg_shipping_cost
 FROM sales_data
 GROUP BY "ReturnStatus"
-ORDER BY avg_shipping_cost;
+--ORDER BY avg_shipping_cost;
+
+SELECT * FROM vw_avg_shipping_cost_for_returned;
 
 -- Most used Shipment Provider
 -- Identify the most frequently used shipment providers and the total volume of completed shipments.
+
+-- CREATE VIEW
+CREATE OR REPLACE VIEW vw_most_used_shipment_provider AS
 
 SELECT
     "ShipmentProvider",
     COUNT(*) AS shipment_completed
 FROM sales_data
-GROUP BY "ShipmentProvider"
-ORDER BY shipment_completed DESC;
+GROUP BY "ShipmentProvider";
+--ORDER BY shipment_completed DESC;
+
+SELECT * FROM vw_most_used_shipment_provider;
 
 -- ==================================================
 -- 4. GEOGRAPHIC & PAYMENT INSIGHTS
@@ -141,16 +191,24 @@ ORDER BY shipment_completed DESC;
 -- Analyze the average spending and total revenue generated by each payment method 
 -- to understand customer transaction preferences.
 
+-- CREATE VIEw
+CREATE OR REPLACE VIEW vw_payment_method_performance AS
+
 SELECT 
     "PaymentMethod",
     ROUND(AVG("TotalRevenue"::NUMERIC), 2) AS avg_total_revenue,
     ROUND(SUM("TotalRevenue"::NUMERIC), 2) AS total_revenue
 FROM sales_data
-GROUP BY "PaymentMethod"
-ORDER BY avg_total_revenue DESC;
+GROUP BY "PaymentMethod";
+--ORDER BY avg_total_revenue DESC;
+
+SELECT * FROM vw_payment_method_performance;
 
 -- Top 5 Countries by Order Volume
 -- Identify the top 5 countries based on order count and their respective total revenue generation.
+
+-- CREATE VIEW
+CREATE OR REPLACE VIEW vw_top_5_countries_by_order_volume AS
 
 SELECT
     "Country",
@@ -160,3 +218,5 @@ FROM sales_data
 GROUP BY "Country"
 ORDER BY orders_completed DESC
 LIMIT 5;
+
+SELECT * FROM vw_top_5_countries_by_order_volume;
--- src/online_sales.py
+++ src/online_sales.py
@@ -9,7 +9,7 @@ def clean_dataset(file_path):
 
     df = pd.read_csv(file_path)
 
-    print(f"Loaded Dataset: {df.shape[0]} rows, {df.shape[1]} columns")
+    print(f"Loaded Dataset: {df.shape[0]} rows, {df.shape[1]} columns\n")
 
     #print(df.head())
     #print(df.info())
@@ -38,7 +38,7 @@ def clean_dataset(file_path):
     df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
     df['Time'] = df['InvoiceDate'].dt.time
     df['InvoiceDate'] = df['InvoiceDate'].dt.date
-    print("Converted 'InvoiceDate' to datetime")
+    print("Converted 'InvoiceDate' to date_time and separated 'Time' into another column")
 
     # Typos
     # This don't have issues
@@ -51,16 +51,19 @@ def clean_dataset(file_path):
     #print(df['WarehouseLocation'].unique())
     #print(df['OrderPriority'].unique())
 
-    # Payment methos has a typo
+    # Payment methods has a typo
     #print(df['PaymentMethod'].unique())
+
     cleanup_map = {
         'paypall': 'PayPal'
     }
+
     df['PaymentMethod'] = df['PaymentMethod'].replace(cleanup_map)
+    
     #print(df['PaymentMethod'].unique())
     print("Changed typo on 'PaymentMethod'")
 
-    # Currency cleaning for 'UnitPrice' and 'ShippingCost'
+    # Currency cleaning for 'UnitPrice', 'ShippingCost' and 'Discount'
     df['UnitPrice'] = pd.to_numeric(df['UnitPrice'])
     df['ShippingCost'] = pd.to_numeric(df['ShippingCost'])
     df['Discount'] = pd.to_numeric(df['Discount'])
@@ -77,8 +80,11 @@ def clean_dataset(file_path):
     df = df[df['UnitPrice'] > 0]
     print("Filtered negatives on 'Quantity' and 'UnitPrice'")
 
+    print()
+
     print("Final info on dataframe")
     print(df.info())
+    print()
 
     # Return the dataframe
     return df
@@ -92,13 +98,16 @@ def get_db_engine():
     port = os.getenv('DB_PORT')
     db = os.getenv('DB_NAME')
 
-    print(f"User: {user}, Port: {port}")
+    print("Database Connection")
+    print(f"User: {user}, Port: {port}\n")
 
     # Return the engine
     return create_engine(f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}')
 
 
 def load_into_sql(df, engine):
+    
+    # We name the table, no need for a variable, but it looks cool
     table_name = 'sales_data'
     df.to_sql(table_name, engine, if_exists='replace', index=False)
     
@@ -106,8 +115,14 @@ def load_into_sql(df, engine):
 
 
 if __name__ == "__main__":
-    clean_df = clean_dataset('online_sales_dataset.csv')
-    clean_df.to_csv('cleaned_online_sales_dataset.csv', index=False)
+    
+    clean_df = clean_dataset('./data/online_sales_dataset.csv')
+    print("Cleaned DataFrame\n")
 
-    #engine = get_db_engine()
-    #load_into_sql(clean_df, engine)
+    # Convert df to csv just in case
+    clean_df.to_csv('./data/cleaned_online_sales_dataset.csv', index=False)
+    print("CSV file created as './data/cleaned_online_sales_dataset.csv'\n")
+
+    engine = get_db_engine()
+
+    load_into_sql(clean_df, engine)
